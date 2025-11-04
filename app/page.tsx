825337e951e9fb96326de00fdc5556aea78978df
Melik1986 <melikmusinian@gmail.com>
2025-11-04 11:35:45 +0100

    chore: initial import (history cleared)

---
+++ app/page.tsx
@@ -0,0 +1,60 @@
+import { fetchCars } from "@utils";
+import { HomeProps } from "@types";
+import { fuels, yearsOfProduction } from "@constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
+
+export default async function Home({ searchParams }: HomeProps) {
+  const params = await searchParams;
+
+  const allCars = await fetchCars({
+    manufacturer: params.manufacturer || "",
+    year: params.year || 2022,
+    fuel: params.fuel || "",
+    limit: params.limit || 10,
+    model: params.model || "",
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1;
+
+  return (
+    <main className='overflow-hidden'>
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(params.limit || 10) / 10}
+              isNext={(params.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>Please try again later or adjust your search filters.</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
